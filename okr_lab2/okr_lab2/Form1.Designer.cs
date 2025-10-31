4cf15bfa5e8e40ffa5bdf76d669617d06d77b551
arsex <arsentar1001@gmail.com>
2025-10-31 10:35:30 +0200

    design

---
+++ okr_lab2/okr_lab2/Form1.Designer.cs
@@ -0,0 +1,283 @@
+﻿namespace okr_lab2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            button1 = new Button();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            label9 = new Label();
+            label10 = new Label();
+            label11 = new Label();
+            label12 = new Label();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            label_bal = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 30F);
+            label1.Location = new Point(297, 56);
+            label1.Name = "label1";
+            label1.Size = new Size(176, 67);
+            label1.TabIndex = 0;
+            label1.Text = "Crypto";
+            label1.Click += label1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(200, 126);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(125, 27);
+            textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(434, 126);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(125, 27);
+            textBox2.TabIndex = 2;
+            textBox2.TextChanged += textBox2_TextChanged;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI", 25F);
+            button1.Location = new Point(200, 164);
+            button1.Name = "button1";
+            button1.Size = new Size(359, 75);
+            button1.TabIndex = 3;
+            button1.Text = "Обміняти";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(200, 242);
+            label2.Name = "label2";
+            label2.Size = new Size(60, 20);
+            label2.TabIndex = 4;
+            label2.Text = "Маркет";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(200, 276);
+            label3.Name = "label3";
+            label3.Size = new Size(106, 20);
+            label3.TabIndex = 5;
+            label3.Text = "Назва валюти";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(331, 276);
+            label4.Name = "label4";
+            label4.Size = new Size(108, 20);
+            label4.TabIndex = 6;
+            label4.Text = "Ціна валюти $";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(200, 323);
+            label5.Name = "label5";
+            label5.Size = new Size(55, 20);
+            label5.TabIndex = 7;
+            label5.Text = "Bitcoin";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(200, 354);
+            label6.Name = "label6";
+            label6.Size = new Size(72, 20);
+            label6.TabIndex = 8;
+            label6.Text = "Ethereum";
+            label6.Click += label6_Click;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(200, 385);
+            label7.Name = "label7";
+            label7.Size = new Size(54, 20);
+            label7.TabIndex = 9;
+            label7.Text = "Solana";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(331, 133);
+            label8.Name = "label8";
+            label8.Size = new Size(33, 20);
+            label8.TabIndex = 10;
+            label8.Text = "BTC";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(565, 133);
+            label9.Name = "label9";
+            label9.Size = new Size(45, 20);
+            label9.TabIndex = 11;
+            label9.Text = "USDT";
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Location = new Point(331, 323);
+            label10.Name = "label10";
+            label10.Size = new Size(61, 20);
+            label10.TabIndex = 12;
+            label10.Text = "110 000";
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Location = new Point(331, 355);
+            label11.Name = "label11";
+            label11.Size = new Size(45, 20);
+            label11.TabIndex = 13;
+            label11.Text = "3 800";
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.Location = new Point(331, 391);
+            label12.Name = "label12";
+            label12.Size = new Size(33, 20);
+            label12.TabIndex = 14;
+            label12.Text = "190";
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.White;
+            button2.ForeColor = Color.Black;
+            button2.Location = new Point(169, 318);
+            button2.Name = "button2";
+            button2.Size = new Size(25, 25);
+            button2.TabIndex = 15;
+            button2.Text = ">";
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.White;
+            button3.ForeColor = Color.Black;
+            button3.Location = new Point(169, 349);
+            button3.Name = "button3";
+            button3.Size = new Size(25, 25);
+            button3.TabIndex = 16;
+            button3.Text = ">";
+            button3.UseVisualStyleBackColor = false;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.BackColor = Color.White;
+            button4.ForeColor = Color.Black;
+            button4.Location = new Point(169, 380);
+            button4.Name = "button4";
+            button4.Size = new Size(25, 25);
+            button4.TabIndex = 17;
+            button4.Text = ">";
+            button4.UseVisualStyleBackColor = false;
+            // 
+            // label_bal
+            // 
+            label_bal.AutoSize = true;
+            label_bal.Location = new Point(690, 9);
+            label_bal.Name = "label_bal";
+            label_bal.Size = new Size(98, 20);
+            label_bal.TabIndex = 18;
+            label_bal.Text = "Баланс USDT";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label_bal);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(label12);
+            Controls.Add(label11);
+            Controls.Add(label10);
+            Controls.Add(label9);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(button1);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Критпо біржа";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Button button1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private Label label9;
+        private Label label10;
+        private Label label11;
+        private Label label12;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Label label_bal;
+    }
+}
---
+++ okr_lab2/okr_lab2/Form1.cs
@@ -0,0 +1,35 @@
+namespace okr_lab2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
